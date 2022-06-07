45bdb7f171b763c6d75792e08765ba94b5b26293
hanissarizki <hanissarizki@gmail.com>
2022-06-08 02:54:31 +0700

    Revert "Revert "Merge branch 'main' of https://github.com/mreyhanf/OnSport""
    This reverts commit 5137e41b2edbd5cb4ebed79b8ea0dfcf8c858415.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Carbon;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -16,13 +19,45 @@ class HomeController extends Controller
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
+     /**
+     * Show Active Created Event Olahraga view.
      */
-    public function index()
+    public function activeCreatedEventOlahraga()
     {
-        return view('home');
+        $user = Auth::user();
+        $events = DB::table('eo')->where('usernamehost', $user->username)->where('tanggal', '>=', Carbon::today())->orderBy('tanggal', 'desc')->orderBy('jam', 'desc')->get();
+        $jumlahpartisipan = [];
+        foreach ($events as $event) {
+            $partisipan = DB::table('partisipan')->where('idevent', $event->idevent)->count();
+            array_push($jumlahpartisipan, $partisipan);
+        }
+
+        return view('home',['events' => $events,'jumlahpartisipan' => $jumlahpartisipan]);
+    }
+
+    /**
+     * Show Active Joined Event Olahraga view.
+     */
+    public function activeJoinedEventOlahraga()
+    {
+        $user = Auth::user();
+        //alternatif 1 ambil dari eoikt
+        // $events = DB::table('eoikt')->where('username', $user->username)->get();
+
+        //alternatif 2 ambil langsung dari eo. eoikt & eorqt sepertinya dihapus saja, tidak diperlukan
+        $ideventpartisipan = DB::table('partisipan')->where('username', $user->username)->get();
+        $eventpartisipan = [];
+        foreach($ideventpartisipan as $ideventpar) {
+            $eventdata = DB::table('eo')->where('idevent', $ideventpar->idevent)->where('tanggal', '>=', Carbon::today())->first();
+            array_push($eventpartisipan, $eventdata);
+        }
+
+        $jumlahpartisipan = [];
+        foreach ($eventpartisipan as $eventpar) { //currently pakai yang dari eo alternatif 2
+            $partisipan = DB::table('partisipan')->where('idevent', $eventpar->idevent)->count();
+            array_push($jumlahpartisipan, $partisipan);
+        }
+
+        return view('home',['events' => $eventpartisipan,'jumlahpartisipan' => $jumlahpartisipan]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8" style="padding-bottom: 15px">
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
@@ -23,11 +23,100 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
                     You are logged in!
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<div class="container">
+
+    <h3 style="padding: 8px; font-weight: bold;">Active Events</h3>
+
+    @if ($events->isEmpty()) <!-- ($events->isEmpty()) -->
+    <div style="padding: 15px;
+    position: absolute;
+    top: 65%;
+    left: 50%;
+    -ms-transform: translateX(-50%) translateY(-50%);
+    -webkit-transform: translate(-50%,-50%);
+    transform: translate(-50%,-50%);">
+        <span style="font-size: 20px; color: rgb(88, 88, 88)">No events yet</span>
+    </div>
+
+@endif
+@if ($events->isNotEmpty()) <!-- ($events->isNotEmpty()) -->
+    @php
+    $carddeckamount = ceil($events->count() / 3); // jumlah card-deck atau baris
+    $eventsindex = 0;
+    $lasteventsindex = $events->count() - 1;
+    for ($i = 1; $i <= $carddeckamount; $i++) {
+        echo '<div class="row card-deck mb-4">';
+        for ($j = $eventsindex; $j <= $eventsindex + 2 && $j <= $lasteventsindex; $j++) {
+            // card deck consisting of 3 cards max
+                $gambar = $events[$j]->gambar == "" ? "events_image/events_placeholder.png" :  $events[$j]->gambar;
+                $judul = strlen($events[$j]->judulevent) > 21 ? substr($events[$j]->judulevent, 0, 21) . "..." : $events[$j]->judulevent;
+                echo '<div class="col-sm-12 col-xl-4">
+                <div class="card" style="min-height: 324px; max-width: 400px; min-width: 200px">
+                    <a href="/event/details/' . $events[$j]->idevent . '">
+                        <img class="card-img-top" src="/' . $gambar . '" alt="" height="178">
+                    </a>
+                    <div class="card-body">
+                        <h5 class="card-title" style="letter-spacing: 0.2px; font-weight: bolder"><a href="/event/details/' . $events[$j]->idevent . '" style="text-decoration: none; color: rgb(29, 153, 255)">' . $judul . '</a></h5>
+                        <div class="row">
+                            <div class="col-6">
+                                <small>Kategori: ' . $events[$j]->kategori . '</small>
+                            </div>
+                            <div class="col-6">
+                                <small>Tanggal: ' . date('d-m-Y', strtotime($events[$j]->tanggal)) . '</small>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-6">
+                                <small>Slot: ' . $jumlahpartisipan[$j] . '/' . $events[$j]->kuotapartisipan . '</small>
+                            </div>
+                            <div class="col-6">
+                                <small>Level: ' . $events[$j]->levelkeahlian . '</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>';
+        }
+        echo '</div>';
+        $eventsindex += 3;
+    }
+    @endphp
+@endif
+
+<hr />
+
+<h3 style="padding: 15px; font-weight: bold ">Recommendations</h3>
+
+</div>
+
+<script>
+    const collection_card_title = document.getElementsByClassName("card-title");
+    for (let i = 0; i < collection_card_title.length; i++) {
+        collection_card_title[i].addEventListener('mouseover', event => {
+            const {target} = event;
+            mouseoverChild(target);
+        });
+
+        collection_card_title[i].addEventListener('mouseout', event => {
+            const {target} = event;
+            mouseoutChild(target);
+        });
+
+        function mouseoverChild(element){
+            element.style.color = 'rgb(0, 89, 206)';
+        }
+
+        function mouseoutChild(element){
+            element.style.color = 'rgb(29, 153, 255)';
+        }
+    }
+</script>
+
 @endsection
