eb70497ff87e5062e913a04cb7c424280228abbe
Muhammad Reyhan Fitriyan <muhammadreyhanfitriyan@gmail.com>
2022-06-10 14:37:03 +0700

    menambah favicon

--- resources/views/layouts/notabs.blade.php
+++ resources/views/layouts/notabs.blade.php
@@ -26,6 +26,12 @@
     <!-- flatpickr -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
 </head>
 <body>
     <div id="app">
